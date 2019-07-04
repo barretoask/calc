65483826406b95e35423f3ce79f139264719c1bb
pedro <pedrobarretooh77@gmail.com>
2019-07-03 21:46:18 -0400

    cria calc

---
+++ calc.js
@@ -0,0 +1,13 @@
+// comentario de uma linha
+
+let nome_da_variavel = 'valor da variavel'
+const nome_da_constante = 'valor da constante'
+
+//console.long()//
+
+
+function minha_funcao ()  {
+
+    alert('Oi')
+    
+}
